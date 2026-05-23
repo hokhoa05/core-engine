8e569796fc3c33a50b4674c043fe1039230d8906
hokhoa05 <khoaholtv0304@gmail.com>
2026-05-24 01:54:37 +0700

    feat(models): implement memory-optimized structs for Order and Trade

--- internal/models/types.go
+++ internal/models/types.go
@@ -1,8 +1,22 @@
 package models
 
+type Side int8
+
+const (
+	Buy Side = 1
+	Sell Side = -1
+)
+
 type Order struct {
 	ID    string
 	Price uint64
 	Qty   uint64
-	Side  int
+	Side  Side
+}
+
+type Trade struct {
+	MakeOrderID uint64
+	TakerOrderID uint64
+	Price uint64
+	Qty uint64
 }
