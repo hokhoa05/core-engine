5220d3dc0a8cd1a86b8c6834ebf0639b20a23059
hokhoa05 <khoaholtv0304@gmail.com>
2026-06-16 23:05:52 +0700

    feat(grpc): create grpc server

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hokhoa05/core-engine/internal/matching"
+	"github.com/hokhoa05/core-engine/internal/server"
+)
 
 func main() {
 	fmt.Println("Core Engine: Distributed Order Matching System is initializing...")
+
+	runner := matching.NewEngineRunner(10000)
+	go runner.Start()
+
+	server.StartGRPCServer(":50051", runner)
 }
---
+++ internal/server/grpc_server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/hokhoa05/core-engine/internal/matching"
+	"github.com/hokhoa05/core-engine/internal/models"
+	pb "github.com/hokhoa05/core-engine/pb/proto"
+	"google.golang.org/grpc"
+)
+
+type grpcServer struct {
+	pb.UnimplementedMatchingEngineServer
+	runner *matching.EngineRunner
+}
+
+func NewGrpcServer(runner *matching.EngineRunner) *grpcServer {
+	return &grpcServer{
+		runner: runner,
+	}
+}
+
+func (s *grpcServer) PushOrder(ctx context.Context, req *pb.OrderRequest) (*pb.OrderResponse, error) {
+	log.Printf("[gRPC] Received Order: %d from User: %d", req.OrderId, req.UserId)
+
+	side := models.Buy
+	if req.Side == "SELL" {
+		side = models.Sell
+	}
+
+	cmd := matching.Command{
+		Type: matching.CmdPlaceOlder,
+		Order: models.Order{
+			ID:    uint64(req.OrderId),
+			Side:  side,
+			Price: uint64(req.Price),
+			Qty:   uint64(req.Qty),
+		},
+	}
+
+	s.runner.PushCommand(cmd)
+
+	return &pb.OrderResponse{
+		Success: true,
+		Message: "Order accepted by Core Engine",
+	}, nil
+}
+
+func StartGRPCServer(port string, runner *matching.EngineRunner) {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Cannot open port: %s: %v", port, err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMatchingEngineServer(s, NewGrpcServer(runner))
+
+	log.Printf("gRPC Server is listening to port %s...", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("gRPC Server down: %v", err)
+	}
+}
